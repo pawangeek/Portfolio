c1ffddf4d1d63c399dc508c60cf349aefb521040
pawangeek <pawanjain.43210@gmail.com>
2024-12-21 16:30:38 +0530

    fix: added metas

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -29,7 +29,7 @@ const Skills = () => {
 
   const listItemVariants = {
     hidden: { opacity: 0, y: 20 },
-    visible: { opacity: 1, y: 0, transition: { duration: 0.5 } },
+    visible: { opacity: 1, y: 0, transition: { duration: 0.3 } },
   };
 
   return (
